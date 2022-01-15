5c4a2ae8335ec30db396333beca6412a3534c2d7
erinszabo <55733370+erinszabo@users.noreply.github.com>
2022-01-15 12:36:35 -0800

    c issue

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
-// #include <unistd.h>
+#include <unistd.h>
 #include <stdio.h>
-
-
 
 //int x = 12;
 //float y = 3.14;
 //char* s = "Hello, world!";
 
-int main(void){
-  printf("test");  
-  // return 0;
+
+int main() {
+  // print x and y string
+   printf("Hello, world!");
+  // print multiples of 5
+   return 0;
 }
