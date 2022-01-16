6c4818ad288c58a2c0cfaf5b3e68f8e2671d4ffa
erinszabo <55733370+erinszabo@users.noreply.github.com>
2022-01-16 14:34:35 -0800

    open and close file in readfile

--- readfile.c
+++ readfile.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iterator>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <malloc.h>
+#include <fcntl.h>
 
-int main(char* argv[]) {
-    File *file_to_read;
-    file_to_read = open(argv[], "r");
-    // print each line of the file
+
+
+int main(int argc, char *argv[]) {
+    int file_to_read;
+    file_to_read = open(argv[1], argc);
+    // read(file_to_read);
+    printf("test\n");
     close(file_to_read);
     return 0;
 }
