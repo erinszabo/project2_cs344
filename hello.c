a481992467ca3579621cd464c8019fe30f532a14
erinszabo <55733370+erinszabo@users.noreply.github.com>
2022-01-15 19:34:01 -0800

    fix and add new lines to hello

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <iterator>
+// #include <iterator>
 
 int x = 12, n;
 float y = 3.14;
 char* s = "Hello, world!";
 
 int main() {
-    printf("x is %d, y is %f", x, y);
-    printf("%s", s);
+    printf("x is %d, y is %f\n", x, y);
+    printf("%s\n", s);
     // print multiples of 5
     for( n = 0; n <= 5; ++n){
-        printf("%d x 5 = %d", n, n * 5);
+        printf("%d x 5 = %d\n", n, n * 5);
     }
     return 0;
 }
